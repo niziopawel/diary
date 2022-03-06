232c35b6c1b4a507572775b4e91acc2aa1789489
niziopawel <niziopawel2@gmail.com>
2022-03-06 15:19:38 +0100

    add User model spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password { Faker::Internet.password min_length: 8 }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user = create(:user)
+  end
+
+  it 'valid with valid attributes' do
+    expect(@user).to be_valid
+  end
+
+  it 'invalid with invalid email' do
+    invalid_emails = ['', '   ', 'asdf', 'email.', 'email@', 'email.com']
+
+    invalid_emails.each do |email|
+      @user.email = email
+
+      expect(@user).not_to be_valid
+    end
+  end
+
+  it 'invalid with empty password' do
+    @user.password = ''
+
+    expect(@user).not_to be_valid
+  end
+
+  it 'invalid with password length less than 0' do
+    @user.password = 'aszcz'
+
+    expect(@user).not_to be_valid
+  end
+
+  it 'invalid without unique email' do
+    user_duplicate = create(:user).dup
+
+    expect(user_duplicate).not_to be_valid
+  end
 end
