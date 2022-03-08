c27783d6bd5b418372ea0ae9ced27ada2bdf9ae8
niziopawel <niziopawel2@gmail.com>
2022-03-08 23:24:59 +0100

    add editing and deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: %i[edit update destroy]
   include ActionView::RecordIdentifier
+
   def index
     @post = Post.new
     @posts = current_user.posts.order(created_at: :desc)
@@ -20,7 +23,9 @@ class PostsController < ApplicationController
         format.html { redirect_to posts_path }
       else
         format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(dom_id(Post.new), partial: 'posts/form', locals: { post: @post })
+          render turbo_stream: turbo_stream.replace(dom_id(Post.new),
+                                                    partial: 'posts/form',
+                                                    locals: { post: @post, title: 'Create post' })
         end
       end
     end
@@ -28,9 +33,31 @@ class PostsController < ApplicationController
 
   def edit; end
 
-  def update; end
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to posts_path, notice: 'Post was successfully updated.' }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(dom_id(@post),
+                                                    partial: 'posts/form',
+                                                    locals: { post: @post, title: 'Create post' })
+        end
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to posts_path, notice: 'Post was successfully destroyed.'
+  end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:city, :notice)
